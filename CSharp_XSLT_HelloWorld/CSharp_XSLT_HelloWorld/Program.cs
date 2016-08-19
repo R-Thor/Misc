74f63938802afeadc5dff24c16956586fe7aa70c
R-Thor <swaybo@yahoo.com>
2016-08-19 02:25:28 -0400

    Committing XSLT C# Working Transform Hello World

---
+++ CSharp_XSLT_HelloWorld/CSharp_XSLT_HelloWorld/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+
+namespace CSharp_XSLT_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XslCompiledTransform xslt = new XslCompiledTransform();
+            string xmlfile = "HelloWorld.xml";
+            string textfile = "HelloWorld.html";
+            xslt.Load("HelloWorldConsole.xslt");
+            xslt.Transform(xmlfile, textfile);
+            FileInfo fi = new FileInfo(textfile);
+            FileStream fs = fi.OpenRead();
+            byte[] filebytes = new byte[fs.Length];
+            fs.Read(filebytes,0, filebytes.Length);
+            string output = Encoding.UTF8.GetString(filebytes);
+            Console.Write(output);
+            //XslCompiledTransform xslt = new XslCompiledTransform();
+            //xslt.Load(typeof(Transform));
+
+            ////// Execute the transformation and output the results to a file.
+            ////xslt.Transform("books.xml", "discount_books.html");
+            //XPathDocument myXPathDoc = new XPathDocument(myXmlFile);
+            //XslTransform myXslTrans = new XslTransform();
+            //myXslTrans.Load(myStyleSheet);
+            //XmlTextWriter myWriter = new XmlTextWriter("result.html", null);
+            //myXslTrans.Transform(myXPathDoc, null, myWriter);
+        }
+    }
+}
